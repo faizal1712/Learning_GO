242e88e8704eda68faf41460409905c5d78f1587
faizal1712 <76579749+faizal1712@users.noreply.github.com>
2021-03-02 14:05:18 +0530

    Edited some files

--- Middleware/main.go
+++ Middleware/main.go
@@ -4,10 +4,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -18,23 +21,27 @@ type userData struct {
 	ID       int    `json:ID`
 	Name     string `json:name`
 	Password string `json:password`
+	Role     string `json:role`
 }
 
 type tokenData struct {
 	Token string `json:token`
 }
 
-const (
-	host     = "localhost"
-	port     = "5432"
-	user     = "postgres"
-	password = "faizu1712"
-	dbname   = "project1"
-)
-
-var myKey = []byte("faizu17121999")
+func goDotEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	return os.Getenv(key)
+}
 
 func getdatabase() *gorm.DB {
+	host := goDotEnvVariable("host")
+	user := goDotEnvVariable("user")
+	password := goDotEnvVariable("password")
+	dbname := goDotEnvVariable("dbname")
+	port := goDotEnvVariable("port")
 	var dsn string = "host=" + host + " user=" + user + " password=" + password + " dbname=" + dbname + " port=" + port + " sslmode=disable" // or "root:@tcp(127.0.0.1:3306)/gorm_mysql?parseTime=true"
 	conn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -66,8 +73,11 @@ func router() {
 	var muxrouter *mux.Router = mux.NewRouter()
 	muxrouter.HandleFunc("/signin", fetchUser).Methods("POST")
 	muxrouter.HandleFunc("/signup", addUser).Methods("POST")
-	muxrouter.HandleFunc("/", homePage).Methods("POST")
-	muxrouter.Use(loggingMiddleware)
+	muxrouter.HandleFunc("/superadmin", superAdminHomePage).Methods("GET")
+	muxrouter.HandleFunc("/admin", adminHomePage).Methods("GET")
+	muxrouter.HandleFunc("/user", userHomePage).Methods("GET")
+	muxrouter.HandleFunc("/", loggingMiddleware(homePage)).Methods("POST")
+	// muxrouter.Use(loggingMiddleware)
 	http.ListenAndServe(":8080", muxrouter)
 }
 
@@ -80,21 +90,53 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	res, _ := json.Marshal(struct {
 		IsError bool
 		Msg     string
-	}{false, "Hello"})
+	}{false, "Hello public"})
 	w.Write(res)
 }
 
-func loggingMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func userHomePage(w http.ResponseWriter, r *http.Request) {
+	res, _ := json.Marshal(struct {
+		IsError bool
+		Msg     string
+	}{false, "Hello user"})
+	w.Write(res)
+}
+
+func superAdminHomePage(w http.ResponseWriter, r *http.Request) {
+	res, _ := json.Marshal(struct {
+		IsError bool
+		Msg     string
+	}{false, "Hello superadmin"})
+	w.Write(res)
+}
+
+func adminHomePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("add")
+	res, _ := json.Marshal(struct {
+		IsError bool
+		Msg     string
+	}{false, "Hello admin"})
+	w.Write(res)
+}
+
+func loggingMiddleware(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 
 		bodydata, _ := ioutil.ReadAll(r.Body)
 		var tokendata tokenData
 		_ = json.Unmarshal(bodydata, &tokendata)
-
+		if tokendata.Token == "" {
+			handler.ServeHTTP(w, r)
+			return
+		}
+		// cookie, _ := r.Cookie("Token")
+		// tokendata.Token = cookie.Value
 		token, err := jwt.Parse(string(tokendata.Token), func(token *jwt.Token) (interface{}, error) {
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 				return nil, fmt.Errorf("There was an error")
 			}
+			myKeyString := goDotEnvVariable("myKey")
+			myKey := []byte(myKeyString)
 			return myKey, nil
 		})
 
@@ -102,16 +144,24 @@ func loggingMiddleware(next http.Handler) http.Handler {
 			fmt.Fprintf(w, err.Error())
 		}
 
-		fmt.Println(r.RequestURI)
+		// fmt.Println(r.RequestURI, token.Claims, token.Header, token.Method, token.Raw, token.Signature, token.Valid)
 		if token.Valid {
-			// Call the next handler, which can be another middleware in the chain, or the final handler.
-			next.ServeHTTP(w, r)
+			role := token.Claims.(jwt.MapClaims)["Role"].(string)
+			if role == "SuperAdmin" {
+				http.Redirect(w, r, "/superadmin", http.StatusSeeOther)
+				return
+			} else if role == "Admin" {
+				http.Redirect(w, r, "/admin", http.StatusSeeOther)
+				return
+			} else if role == "User" {
+				http.Redirect(w, r, "/user", http.StatusSeeOther)
+				return
+			}
+			// next.ServeHTTP(w, r)
 		} else {
 			fmt.Println("not logged in")
 		}
-
-		// Do stuff here
-	})
+	}
 }
 
 func fetchUser(w http.ResponseWriter, r *http.Request) {
@@ -155,13 +205,18 @@ func fetchUser(w http.ResponseWriter, r *http.Request) {
 			claims := token.Claims.(jwt.MapClaims)
 			claims["Name"] = fetchUser.Name
 			claims["Id"] = fetchUser.ID
-
+			claims["Role"] = fetchUser.Role
+			myKeyString := goDotEnvVariable("myKey")
+			myKey := []byte(myKeyString)
 			tokenstring, err := token.SignedString(myKey)
 
 			if err != nil {
 				fmt.Println(err)
 			}
-			// r.Header.Set("Token", tokenstring)
+			// http.SetCookie(w, &http.Cookie{
+			// 	Name:  "Token",
+			// 	Value: tokenstring,
+			// })
 			res, _ = json.Marshal(struct {
 				IsError bool
 				Count   int
@@ -186,8 +241,6 @@ func addUser(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	// fmt.Println(user)
-
 	insert := db.Create(&user)
 	if insert.Error != nil {
 		panic(insert.Error)
